cba25f52213fdff320805c7512ff4e48cc646621
Ivan <xspals@mail.ru>
2023-01-12 22:42:35 +0300

    add repos and change some styles

--- src/Components/Content/ContentBlocks/MyProjects/Project/Project.jsx
+++ src/Components/Content/ContentBlocks/MyProjects/Project/Project.jsx
@@ -3,13 +3,16 @@ import classes from './Project.module.css';
 
 const Project = (props) => {
 
-    const { name, desctiption, src, img } = props;
+    const { name, desctiption, src, gitHub, img } = props;
 
     return <div className={classes.projectBlock}>
         <div className={classes.project}>
             <h4 className={classes.nameProject}>{name}</h4>
             <p className={classes.descriptionProject}>{desctiption}</p>
-            <a className={classes.sourceProject} href={src} target="_blank" rel="noreferrer">Перейти на сайт</a>
+            <div className={classes.sourcesContainer}>
+                <a className={classes.sourceProject} href={src} target="_blank" rel="noreferrer">Перейти на сайт</a>
+                <a className={classes.gitHubSource} href={gitHub} target="_blank" rel="noreferrer">Репозиторий</a>
+            </div>
         </div>
         <img className={classes.arrowImage} src="https://img.icons8.com/sf-regular/96/000000/chevron-right.png" alt="" />
         <div className={classes.projectImage}>
--- src/redux/resources/projectsObj.js
+++ src/redux/resources/projectsObj.js
@@ -3,12 +3,14 @@ export const projects = [
         name: 'Пицца-сайт',
         desctiption: 'Веб-приложение, которое было разработано на основе библиотеки Material UI. Имеется некоторый функционал, как на многих других сайтах.',
         src: 'https://Beaster1332.github.io/React-pizza',
+        gitHub: 'https://github.com/Beaster1332/React-pizza',
         img: '/ProjectsImages/Pizza.png'
     },
     {
         name: 'Поисковик кино',
         desctiption: 'Приложение, основанное на неофициальном API портала Кинопоиск. На данном сайте можно найти фильм, который есть в базе Кинопоиска.',
         src: 'https://Beaster1332.github.io/Movie-searcher',
+        gitHub: 'https://github.com/Beaster1332/Movie-searcher',
         img: '/ProjectsImages/Movies.png'
     }
 ]
